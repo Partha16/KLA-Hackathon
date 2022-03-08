834457f37bb6cf1d0fb1ecd4bb9faafaa3391dac
Partha <partha2001cbe@gmail.com>
2022-03-08 10:58:43 +0530

    3rd coomit

--- yaml-parser.py
+++ yaml-parser.py
@@ -3,6 +3,7 @@ import yaml
 from yaml.loader import SafeLoader
 import datetime
 import time as t
+import threading
 
 with open('Milestone1/Milestone1A.yaml','r') as f:
     data = yaml.load(f, Loader=SafeLoader)
@@ -12,31 +13,43 @@ class Sequential:
     
     data = {}
     cur = ''
-    def __init__(self,data,cur = ''):
+    concur = False
+    def __init__(self,data,cur = '',concurrent = False):
         global time
         self.data = data
         self.cur = cur
+        self.concur = concurrent
+        threads = []
         print(str(datetime.datetime.now())+';'+self.cur+' Entry')
         k = data['Activities']
+
         for j in k:
             if k[j]['Type'] =='Task':
-                #print(j)
-                self.exec_func(j,k[j]['Function'],k[j]['Inputs'])
+                if self.concur:
+                    t_temp = threading.Thread(self.exec_func,args=(j,k[j]['Function'],k[j]['Inputs']))
+                    threads.append(t_temp)
+                    t_temp.start()
+                else:
+                    self.exec_func(j,k[j]['Function'],k[j]['Inputs'])
             else:
                 #print(k[j])
-                next = Sequential(data = k[j],cur = self.cur+'.'+j)
-                #self.time += p2.time
-                del next
-
+                if self.concur:
+                    t_temp = threading.Thread(target=Sequential, args = (k[j],self.cur+'.'+j,(k[j]['Execution'] == 'Concurrent')))
+                    threads.append(t_temp)
+                    t_temp.start()
+                else:
+                    next = Sequential(data = k[j],cur = self.cur+'.'+j,concurrent = (k[j]['Execution'] == 'Concurrent'))
+                    #del next
+        for x in threads:
+            x.join()
+        
+        #self.__del__()
     def __del__(self):
         global time 
         print(str(datetime.datetime.now())+';'+self.cur+' Exit')
 
     def TimeFunction(self,i):
-        #global time
-        #time += datetime.timedelta(0,int(i))
         t.sleep(int(i))
-        #print(self.time)
     
     def exec_func(self,name,func,inputs):
         global time
@@ -46,18 +59,11 @@ class Sequential:
             self.TimeFunction(inputs['ExecutionTime'])
         print(str(datetime.datetime.now())+';'+self.cur+'.'+name+' Exit')
 
-class Concurrent:
 
-    data = {}
-    cur = ''
-    def __init__(self,data,cur = ''):
-        global time
-        self.data = data
-        self.cur = cur
 
 time = datetime.datetime.now()
 for i in data:
     print(i)
     if data[i]['Execution'] =='Sequential':
         p1 = Sequential(data[i],i)
-        del p1
+        #del p1
